c30bbc41c0ed5ec18b8551fe2df6518a4071f2b5
HadHanns <139573921+HadHanns@users.noreply.github.com>
2024-09-13 21:18:57 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+  POPUP
+</div> */}
+
+const Home = () => {
+  return (
+    <section className='w-full h-screen relative'>
+      
+    </section>
+  )
+}
+
+export default Home
