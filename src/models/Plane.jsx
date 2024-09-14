4bef428369b4f3d4596785df1ed05dcc18dfe3b3
HadHanns <139573921+HadHanns@users.noreply.github.com>
2024-09-14 15:09:20 +0700

    Customize model

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 import planeScene from '../assets/3d/plane.glb'
+import { useGLTF } from '@react-three/drei'
 
-const Plane = () => {
+const Plane = ({isRotating, ...props }) => {
+    const { scene, animation } = useGLTF(planeScene);
   return (
-    <mesh></mesh>
+    <mesh {...props}>
+        <primitive object={scene}/>
+    </mesh>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { useState, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
@@ -12,6 +12,7 @@ import Plane from '../models/Plane';
 </div> */}
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
 
   //MARK: untuk mengukur ulang Island
   const adjustIslandForScreenSize = () => {
@@ -28,12 +29,28 @@ const Home = () => {
     return [screenScale, screenPosition, rotation]
   }
 
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if(window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0]
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [2, -4, -4]
+    }
+
+    return [screenScale, screenPosition];
+  }
+
+  //MARK: untuk memanggil Screen yang sudah di adjust
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
     <section className='w-full h-screen relative'>
       <Canvas 
-        className='w-full h-screen bg-transparent'
+        className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
         camera={{near: 0.1, far: 1000}}
       >
         <Suspense fallback={<Loader />}>
@@ -49,8 +66,15 @@ const Home = () => {
             position = {islandPosition}
             scale = {islandScale}
             rotation = {islandRotation}
+            isRotating = {isRotating}
+            setIsRotating = {setIsRotating}
           />
-          <Plane />
+          <Plane 
+            isRotating = {isRotating}
+            planeScale = {planeScale}
+            planePosition= {planePosition}
+            rotation = {[0, 20, 0]}
+          />
         </Suspense>
         
       </Canvas>
