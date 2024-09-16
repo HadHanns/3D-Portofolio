2bd0cec10b657c5878ea59e07b3eaeaf5a5930ff
HadHanns <139573921+HadHanns@users.noreply.github.com>
2024-09-16 15:04:10 +0700

    Update Alert Message

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,12 +4,16 @@ import { Canvas } from '@react-three/fiber';
 
 import Fox from '../models/Fox';
 import Loader from '../components/Loader';
+import useAlert from '../hooks/useAlert';
+import Alert from '../components/Alert';
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: '', email: '', message: '' });
   const [isLoading, setIsLoading] = useState(false);
   const [currentAnimation, setCurrentAnimation] = useState('idle')
+
+  const {alert, showAlert, hideAlert} = useAlert();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -45,8 +49,12 @@ const Contact = () => {
     ).then(() => {
       setIsLoading(false);
       // TODO: Show Success Message
-      // TODO: Hide an alert
+      showAlert({show: true, text: 'Message sent succesfully!', type: 'success'});
+
       setTimeout(() => {
+        // TODO: Hide an alert
+        hideAlert();
+
         setCurrentAnimation('idle'); 
         setForm({ name: '', email: '', message: '' });
       },[3000])
@@ -56,11 +64,14 @@ const Contact = () => {
       setCurrentAnimation('idle');
       console.log(error);
       // TODO: Show Error Message
+      showAlert({show: true, text: "I didn't receive your message", type: 'danger'});
     });
   };
 
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
+      {alert.show && <Alert {...alert}/>}
+
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text'>Get in Touch</h1>
 
@@ -134,9 +145,9 @@ const Contact = () => {
           <Suspense fallback={<Loader />}>
             <Fox 
               currentAnimation={currentAnimation}
-              position={[0.5, 0, 0]}
-              rotation={[12.6, -0.6, 0]}
-              scale={[0.7, 0.7, 0.7]}
+              position={[1, 0.3, 0]}
+              rotation={[12.6, -0.8, 0]}
+              scale={[0.5, 0.5, 0.5]}
             />
           </Suspense>
 
